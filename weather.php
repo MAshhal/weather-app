01f2394b8e3a9626b8212d062f711b9db6ecebf0
Muhammad Ashhal <96419703+MAshhal@users.noreply.github.com>
2026-01-19 18:36:17 +0500

    feat: improve weather.php with formatted weather and location output

--- weather.php
+++ weather.php
@@ -14,6 +14,46 @@ $dotenv->load();
 
 $weatherService = WeatherService::create();
 $city = "Lahore";
+
+echo "Getting weather for $city...\n";
 $weather = $weatherService->getCurrentWeather($city);
 
-var_dump($weather);
+$location = $weather['location'];
+$data = $weather['data'];
+
+echo "\n";
+echo "Location\n";
+echo "--------\n";
+foreach ($location as $key => $value) {
+    $formatted_key = match ($key) {
+        'tz_id' => "Timezone",
+        'lat' => 'Latitude',
+        'lon' => 'Longitude',
+        'localtime' => 'Local Time',
+        'localtime_epoch' => 'Local Time (Epoch seconds)',
+        default => ucwords($key)
+    };
+
+    $formatted_value = match ($key) {
+        'lat', 'lon' => "{$value}°",
+        default => $value
+    };
+
+    echo " {$formatted_key} : {$formatted_value} \n";
+}
+
+echo "\n";
+echo "Weather\n";
+echo "-------\n";
+foreach ($data as $key => $value) {
+    $formatted_key = match ($key) {
+        'last_updated_epoch' => 'Last Updated (Epoch seconds)',
+        default => ucwords(str_replace('_', ' ', $key))
+    };
+    $formatted_value = match ($key) {
+        'temperature', 'feels_like' => "{$value}°C",
+        'humidity' => "{$value}%",
+        default => $value
+    };
+    echo " {$formatted_key} : {$formatted_value} \n";
+}
